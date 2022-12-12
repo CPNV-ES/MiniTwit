35067211b4016265218587b20068e9d9440a3980
Yann Menoud <Yann.MENOUD@cpnv.ch>
2022-12-12 11:25:21 +0100

    Add fields into migration file

--- MiniTwit/database/migrations/2022_12_12_101436_create_posts_table.php
+++ MiniTwit/database/migrations/2022_12_12_101436_create_posts_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('text');
+            $table->array('likes');
             $table->timestamps();
         });
     }
