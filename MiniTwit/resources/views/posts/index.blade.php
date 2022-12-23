add81da949e803a4e965e0267f28cbf9a814bb8f
menoudyann <Yann.MENOUD@cpnv.ch>
2022-12-23 14:59:48 +0100

    fix view to show all posts titles

--- MiniTwit/resources/views/posts/index.blade.php
+++ MiniTwit/resources/views/posts/index.blade.php
@@ -1,16 +1,10 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Votre feed....
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <?php foreach ($posts as $post) : ?>
                     <x-jet-welcome>
-                        {{$post->text}}
+                        {{ __($post->text) }}
                     </x-jet-welcome>
                 <?php endforeach; ?>
             </div>
