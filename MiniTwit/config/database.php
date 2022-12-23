5e76e4ca40cf6efe03be7d1bfadb5a570dc19579
Luís Pedro Pinheiro <lpodevco@gmail.com>
2022-12-23 14:00:59 +0100

    Update database.php

--- MiniTwit/config/database.php
+++ MiniTwit/config/database.php
@@ -100,15 +100,6 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
         ]
-
-        'mongodb' => [
-            'driver' => 'mongodb',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-        ]
     ],
 
     /*
