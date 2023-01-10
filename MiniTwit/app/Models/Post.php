d9d64368079cf12146393911fa4440eaef187182
menoudyann <Yann.MENOUD@cpnv.ch>
2023-01-10 19:18:22 +0100

    fixed routes for comments

--- MiniTwit/app/Models/Post.php
+++ MiniTwit/app/Models/Post.php
@@ -11,6 +11,7 @@ class Post extends Model
 
     protected $casts = [
         'likes' => User::class,
+        'created_at' => 'datetime:Y/m/d',
     ];
 
     // public function comments()
--- MiniTwit/resources/views/posts/index.blade.php
+++ MiniTwit/resources/views/posts/index.blade.php
@@ -9,28 +9,24 @@
         <div class="max-w-7xl mx-auto md:px-6 lg:px-8">
             <?php foreach ($posts as $post) : ?>
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg mb-6">
-
                     <div class="max-w-sm rounded overflow-hidden shadow-lg">
                         <div class="px-6 py-4 ">
-                            <div class="font-bold text-xl mb-2">Posted by {{$post->user->name}}</div>
+                            <div class="font-bold text-xl">Posted by {{$post->user->name}}</div>
+                            <div class="mb-2"><small>{{$post->created_at->format('Y-m-d')}}</small></div>
                             <p class="text-gray-700 text-base">
                                 {{$post->text}}
                             </p>
                         </div>
                     </div>
-
                 </div>
             <?php endforeach; ?>
         </div>
-        <div class="container py-10 px-10 mx-0 min-w-full flex flex-col items-center bg-white">
-            @if (Auth::check())
+        <div class="container py-10 px-10 mx-0 min-w-full flex flex-col items-center">
             <form action="{{ route('posts.create') }}" method="GET">
-                <button class="bg-yellow-500 hover:bg-yellow-400 text-white font-bold py-2 px-4 rounded">
-                    Add a new game
+                <button class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded-full">
+                    New Post
                 </button>
             </form>
-            @endif
-
         </div>
     </div>
 </x-app-layout>
